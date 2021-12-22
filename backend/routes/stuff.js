f247ad48add24e986cf96bd9321a3087c5287f63
Cyril <realc972@gmail.com>
2021-12-22 01:20:42 +0100

    ajout multer

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -4,11 +4,13 @@ const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
 
 router.get('/', auth, stuffCtrl.getAllSauces);
-router.post('/', auth, stuffCtrl.createSauce);
+router.post('/', auth, multer, stuffCtrl.createSauce);
 router.get('/:id', auth, stuffCtrl.getOneSauce);
-router.put('/:id', auth, stuffCtrl.modifySauce);
+router.put('/:id', auth, multer, stuffCtrl.modifySauce);
 router.delete('/:id', auth, stuffCtrl.deleteSauce);
 
 
